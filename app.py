e185542990bd024a3e07a876a458e49c37acfbbf
Your Name <you@example.com>
2022-02-11 16:58:48 +0300

    gjhk

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print('Hello')
+
+from tkinter import Variable
+from typing import Any
+from pygame import *
+window = display.set_mode((700, 500))
+display.set_caption('Моя первая игра!')
+
+
+
+picture = transform.scale(image.loand("fon.jpg"), (700, 500))
+
+
+
+
+
+run = True
+while run:
+    time.delay(50)
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+     (Variable) window: Any
+    window.blit(picture,(0, 0))
+
+    display.update()
+        
